642d326bf1d4b9322d365414cca6a470d8be53df
Vin <54696843+DrunkenGoat@users.noreply.github.com>
2023-07-05 13:39:08 +0200

    Implemented Carlys animations

--- Assets/Scripts/TestAnimationsCharacter.cs
+++ Assets/Scripts/TestAnimationsCharacter.cs
@@ -1,90 +1,252 @@
 using Live2D.Cubism.Framework.Motion;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 public class TestAnimationsCharacter : MonoBehaviour
 {
+    public enum ANIMATIONLAYER
+    {
+        BLINKING = 1,
+        BODY_BOUNCE = 2,
+        MOUTH_IDLE = 3,
+        MOUTH_TALKING = 4,
+        EMOTION = 5
+    }
+
+    public enum Emotion { Normal, Angry, Bored, Doubtful, Excited, Null }
+
     CubismMotionController _motionController;
-    public AnimationClip blinking_animation;              //Animation Layer: 1
-    public AnimationClip body_bounce_animation;           //Animation Layer: 2
-    public AnimationClip excited_animation;               //Animation Layer: 5
-    public AnimationClip smile_animation;                 //Animation Layer: 3
-    public AnimationClip happy_talking_animation;         //Animation Layer: 4
-    public AnimationClip sad_talking_animation;           //Animation Layer: 4
 
-    public bool currently_talking; //No logic until now, change to true when currently text appears, change to false when text has finished appearing
-    public bool eager_to_talk = true; //No logic
+    [TextArea]
+    [Tooltip("USe the Keys Q (Reset) W E R T to switch between the Animations")]
+    public string Summary = "This component shouldn't be removed, it does important stuff.";
 
+    [Header("Debug")]
+    [SerializeField] bool disable_debug_messages = false;
+    [SerializeField] bool disable_warning_messages = false;
+    [SerializeField] bool disable_test_animation_keys = false;
+
+    [Header("Debug Variables (Changing them wont do anything)")]
+    public Emotion CurrentEmotion = Emotion.Normal;
+    public bool CurrentlyTalking; //No logic until now, change to true when currently text appears, change to false when text has finished appearing
+
+    [Header("Animations - General - Layer 1 & 2")]
+    [SerializeField] AnimationClip blinking_animation;              //Animation Layer: 1
+    [SerializeField] AnimationClip body_bounce_animation;           //Animation Layer: 2
+    [Header("Animations - Mouths- Layer 3")]
+    [SerializeField] AnimationClip smile_mouth_animation;           //Animation Layer: 3
+    [SerializeField] AnimationClip unhappy_mouth_animation;
+    [Header("Animations - Talking- Layer 4")]
+    [SerializeField] AnimationClip happy_talking_animation;         //Animation Layer: 4
+    [SerializeField] AnimationClip sad_talking_animation;           //Animation Layer: 4
+    [Header("Animations - Emotions- Layer 5")]
+    [SerializeField][Tooltip("Plays on W")] AnimationClip angry_animation;
+    [SerializeField][Tooltip("Plays on E")] AnimationClip bored_animation;
+    [SerializeField][Tooltip("Plays on R")] AnimationClip doubtful_animation;
+    [SerializeField][Tooltip("Plays on T")] AnimationClip excited_animation;               //Animation Layer: 5
+
+    bool emotion_was_changed = false;
 
     private void Start()
     {
         _motionController = GetComponent<CubismMotionController>();
+
         //Start the Starting Animations
-        PlayMotion(blinking_animation, 1, true); //Blinking
-        PlayMotion(body_bounce_animation, 2, true); //Body Bounce
-        PlayMotion(smile_animation, 3, true, priority: CubismMotionPriority.PriorityIdle); //Idle so it can be overwritten by Layer 4 (Talking)
+        PlayMotion(blinking_animation, ANIMATIONLAYER.BLINKING, true); //Blinking
+        PlayMotion(body_bounce_animation, ANIMATIONLAYER.BODY_BOUNCE, true); //Body Bounce
+        PlayMotion(smile_mouth_animation, ANIMATIONLAYER.MOUTH_IDLE, true, priority: CubismMotionPriority.PriorityIdle); //Idle so it can be overwritten by Layer 4 (Talking)
     }
 
     private void Update()
     {
-        UpdateAnim();
+        if (emotion_was_changed) //Continues with the emotion change form the last frame
+        {
+            PlayEmotion(CurrentEmotion);
+            emotion_was_changed = false;
+        }
+        if (!disable_test_animation_keys) UpdateAnim();
     }
 
     //Plays Motions specific to the different animation layers. Each layer can only play one animation. 
     //If one layer overwrites parameters of another layer, the motion with the higher priority takes over
-    public void PlayMotion(AnimationClip animation, int animation_layer, bool looping = false, int priority = CubismMotionPriority.PriorityNormal)
+    public void PlayMotion(AnimationClip animation, ANIMATIONLAYER animation_layer, bool looping = false, int priority = CubismMotionPriority.PriorityNormal)
     {
         if ((_motionController == null) || (animation == null))
         {
             Debug.LogWarning("Animation could not be played, no Motion Controller or Animation");
             return;
         }
-        _motionController.PlayAnimation(animation, isLoop: looping, layerIndex: animation_layer);
+        _motionController.PlayAnimation(animation, isLoop: looping, layerIndex: (int)animation_layer); //Enum os changed to int so PlayAnimation can be called
     }
 
     //Stops Animations, animations take one frame to become stopped
-    public void StopMotion(int animation_layer) 
+    public void StopMotion(ANIMATIONLAYER animation_layer) 
     {
-        _motionController.StopAnimation(1, animation_layer); //I litarally have no fucking clue why there is an animation index (that 3) the function doesnt even use it
+        _motionController.StopAnimation(1, (int)animation_layer); //I litarally have no fucking clue why there is an animation index (that 3) the function doesnt even use it
     }
 
     private void UpdateAnim()
     {
         //Debug Input for testing
         //Keep pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartTalking();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            FinishTalking();
+        }
+        //Press
+        if (Input.GetKeyDown(KeyCode.Q)) 
+        {
+            ChangeEmotion(Emotion.Normal);
+        }
         if (Input.GetKeyDown(KeyCode.W))
         {
-            PlayMotion(happy_talking_animation, 4, true);
+            ChangeEmotion(Emotion.Angry);
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
-            PlayMotion(sad_talking_animation, 4, true);
+            ChangeEmotion(Emotion.Bored);
         }
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            StopMotion(4);
+            ChangeEmotion(Emotion.Doubtful);
         }
-        //Toggle
-        if (Input.GetKeyDown(KeyCode.R)) 
+        if (Input.GetKeyDown(KeyCode.T))
         {
-            eager_to_talk = !eager_to_talk;
-            if (eager_to_talk)
-            {
-                PlayMotion(excited_animation, 5, true); 
-            }
-            else
-            {
-                StopMotion(5);
-            }
-            
+            ChangeEmotion(Emotion.Excited);
         }
     }
 
     public void StartTalking()
     {
-        PlayMotion(happy_talking_animation, 4, true);
+        //if (emotion_changed != Emotion.Null)
+        //{
+        //    ChangeEmotion(emotion_changed);
+        //}
+        StartMouthAnimation(CurrentEmotion);
+        CurrentlyTalking = true;
     }
+    
     public void FinishTalking()
     {
-        StopMotion(4);
+        StopMouthAnimation(CurrentEmotion);
+        CurrentlyTalking = false;
+    }
+
+    public void ChangeEmotion(Emotion next_emotion)
+    {
+        if (CurrentEmotion == next_emotion)
+        {
+            if (!disable_debug_messages) Debug.Log("Skipped ChangeEmotion(), Emotion was the same ");
+            return;
+        }
+        CurrentEmotion = next_emotion;
+        emotion_was_changed = true;
+        PlayEmotion(Emotion.Normal); //Stops Emotion from being played
+        //PlayEmotion(CurrentEmotion); //Moved to the following frames Update
+    }
+
+    void PlayEmotion(Emotion emotion)
+    {
+        switch (emotion)
+        {
+            case Emotion.Null:
+                if (!disable_warning_messages) Debug.LogWarning("Emotion was Null");
+                break;
+
+            case Emotion.Normal: 
+                StopMotion(ANIMATIONLAYER.EMOTION); 
+                break;
+            case Emotion.Doubtful:
+                PlayMotion(doubtful_animation, ANIMATIONLAYER.EMOTION, true);
+                break;
+            case Emotion.Excited:
+                PlayMotion(excited_animation, ANIMATIONLAYER.EMOTION, true);
+                break;
+            case Emotion.Bored:
+                PlayMotion(bored_animation, ANIMATIONLAYER.EMOTION, true);
+                break;
+            case Emotion.Angry:
+                PlayMotion(angry_animation, ANIMATIONLAYER.EMOTION, true);
+                break;  
+            
+            default:
+                if (!disable_warning_messages) Debug.LogWarning("Emotion not found: " + emotion);
+                break;
+        }
+    }
+
+    void StartMouthAnimation(Emotion emotion)
+    {
+        switch (emotion)
+        {
+            case Emotion.Null:
+                if (!disable_warning_messages) Debug.LogWarning("Emotion was Null");
+                break;
+
+            case Emotion.Normal:
+                PlayMotion(happy_talking_animation, ANIMATIONLAYER.MOUTH_TALKING, true);
+                StopMotion(ANIMATIONLAYER.MOUTH_IDLE);
+                break;
+            case Emotion.Doubtful:
+                PlayMotion(sad_talking_animation, ANIMATIONLAYER.MOUTH_TALKING, true);
+                StopMotion(ANIMATIONLAYER.MOUTH_IDLE);
+                break;
+            case Emotion.Excited:
+                PlayMotion(happy_talking_animation, ANIMATIONLAYER.MOUTH_TALKING, true);
+                StopMotion(ANIMATIONLAYER.MOUTH_IDLE);
+                break;
+            case Emotion.Bored:
+                PlayMotion(sad_talking_animation, ANIMATIONLAYER.MOUTH_TALKING, true);
+                StopMotion(ANIMATIONLAYER.MOUTH_IDLE);
+                break;
+            case Emotion.Angry:
+                PlayMotion(sad_talking_animation, ANIMATIONLAYER.MOUTH_TALKING, true);
+                StopMotion(ANIMATIONLAYER.MOUTH_IDLE);
+                break;
+            
+            default:
+                if (!disable_warning_messages) Debug.LogWarning("Emotion not found: " + emotion);
+                break;
+        }
+    }
+
+    void StopMouthAnimation(Emotion emotion)
+    {
+        switch (emotion)
+        {
+            case Emotion.Null:
+                if (!disable_warning_messages) Debug.LogWarning("Emotion was Null");
+                break;
+
+            case Emotion.Normal:
+                PlayMotion(smile_mouth_animation, ANIMATIONLAYER.MOUTH_IDLE, true);
+                StopMotion(ANIMATIONLAYER.MOUTH_TALKING);
+                break;
+            case Emotion.Doubtful:
+                PlayMotion(unhappy_mouth_animation, ANIMATIONLAYER.MOUTH_IDLE, true);
+                StopMotion(ANIMATIONLAYER.MOUTH_TALKING);
+                break;
+            case Emotion.Excited:
+                PlayMotion(smile_mouth_animation, ANIMATIONLAYER.MOUTH_IDLE, true);
+                StopMotion(ANIMATIONLAYER.MOUTH_TALKING);
+                break;
+            case Emotion.Bored:
+                PlayMotion(unhappy_mouth_animation, ANIMATIONLAYER.MOUTH_IDLE, true);
+                StopMotion(ANIMATIONLAYER.MOUTH_TALKING);
+                break;
+            case Emotion.Angry:
+                PlayMotion(unhappy_mouth_animation, ANIMATIONLAYER.MOUTH_IDLE, true);
+                StopMotion(ANIMATIONLAYER.MOUTH_TALKING);
+                break;
+            
+            default:
+                if (!disable_warning_messages) Debug.LogWarning("Emotion not found: " + emotion);
+                break;
+        }
     }
 }
