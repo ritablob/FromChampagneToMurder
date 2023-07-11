e370e2614d9eb1bb77adedfdf9ac174470fb2ed2
ritablob <106591191+ritablob@users.noreply.github.com>
2023-07-12 01:16:19 +0200

    im an idiot

--- Assets/Scripts/ReportCrimeScene.cs
+++ Assets/Scripts/ReportCrimeScene.cs
@@ -7,27 +7,22 @@ using UnityEngine.UI;
 public class ReportCrimeScene : MonoBehaviour
 {
     public List<string> possibleCorrectAnswers = new List<string>();
-    public TMP_Text inputField;
+    public TMP_InputField inputField;
     public GameObject ExitButton;
     public Animator reportAnimator;
     public bool DoesTextMatch()
     { 
         for (int i = 0; i < possibleCorrectAnswers.Count; i++)
         {
-
             if (CompareWords(inputField.text, possibleCorrectAnswers[i]))
             {
                 Debug.Log("its a mathc!!!");
                 return true;
             }
-            Debug.Log(inputField.text == possibleCorrectAnswers[i]);
-            Debug.Log("not a match :(, text: " + "-"+inputField.text+ "-"+ possibleCorrectAnswers[i] + "-");
-            //Debug.Log(inputField.text.GetType() + " " + possibleCorrectAnswers[i].GetType());// it does go through and read the possible answers
         }
         Debug.Log("not a match :(, text: "+ inputField.text);
         ClearTextBox();
         return false;
-
     }
     public void ClearTextBox()
     {
@@ -39,15 +34,8 @@ public class ReportCrimeScene : MonoBehaviour
     }
     bool CompareWords(string wordOne, string wordTwo)
     {
-        wordOne = wordOne.ToLower();
-        wordOne = wordOne.TrimEnd();
-        wordOne = wordOne.TrimStart();
-        wordOne = wordOne.Trim();
-        wordTwo = wordTwo.TrimEnd();
-        wordTwo = wordTwo.TrimStart();
-        wordTwo = wordTwo.Trim();
-        wordTwo = wordTwo.ToLower();
-
+        wordOne = wordOne.ToLower().TrimEnd().TrimStart().Trim();
+        wordTwo = wordTwo.TrimEnd().TrimStart().Trim().ToLower();
         return wordOne == wordTwo;
     }
 }
